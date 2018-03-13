d26673276a3e8b4e9a77f8aa5b783c796ca73557
Suk Hoon Kim <sz8386pr@go.minneapolis.edu>
2018-03-13 15:52:27 -0500

    unittest

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
 
 # Create your tests here.
+
+class TestViewHomePageIsEmptyList(TestCase):
+
+    def test_load_home_page_shows_empty_list(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertFalse(response.context['places']) #empty lists are false
+
+
+
+
+class TestWishList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_view_wishlist(self):
+        response = self.client.get(reverse('place_list'))
+
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        data_rendered = list(response.context['places'])
+
+        data_expected = list(Place.objects.filter(visited=False))
+
+        self.assertCountEqual(data_rendered, data_expected)
