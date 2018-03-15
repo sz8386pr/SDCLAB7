7de2e54eb0b4cab2824323406ba95773afaa3aa2
Scott Kim <31192904+sz8386pr@users.noreply.github.com>
2018-03-15 13:19:51 -0500

    Update models.py

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Place(models.Model):
-    name = models.CharField(max_length=200)
-    visited = models.BooleanField(default=False)
+    name = models.CharField(max_length=200) # User input for the place name to be added onto the wishlist. max_length is the max string length for the input field
+    visited = models.BooleanField(default=False) # If user has visited place
 
     def __str__(self):
         return '%s visited? %s' % (self.name, self.visited)
