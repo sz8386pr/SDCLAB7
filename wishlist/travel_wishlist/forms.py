aa3f397686fb2f37108786ce4c74dc95605bd9bc
Scott Kim <31192904+sz8386pr@users.noreply.github.com>
2018-03-15 13:23:35 -0500

    Update forms.py

--- wishlist/travel_wishlist/forms.py
+++ wishlist/travel_wishlist/forms.py
@@ -3,5 +3,5 @@ from .models import Place
 
 class NewPlaceForm(forms.ModelForm):
     class Meta:
-        model = Place
-        fields = ('name', 'visited')
+        model = Place   # uses the Place class object model from the models.py
+        fields = ('name', 'visited')    #fields name will be name and visited, to match the variables in the Place class object
