6d33b1314fde27ff398850e44f60660f08a39175
Scott Kim <31192904+sz8386pr@users.noreply.github.com>
2018-03-15 13:37:29 -0500

    Update urls.py

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.place_list, name='place_list'),
-    url(r'^visited$', views.places_visited, name='places_visited'),
-    url(r'^isvisited$', views.place_is_visited, name='place_is_visited'),
+    url(r'^$', views.place_list, name='place_list'),    # if url is index/base, invoke place_list() from the views.py
+    url(r'^visited$', views.places_visited, name='places_visited'), # if url is visited, invoke places_visited() from the views.py
+    url(r'^isvisited$', views.place_is_visited, name='place_is_visited'),   # if url is isvisited, invoke place_is_visited() from the views.py
 ]
